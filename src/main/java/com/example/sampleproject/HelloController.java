311b79e550bf07753e185f5d10610c50bc8cbed5
lsh <fantasyz7@naver.com>
2022-07-24 12:14:55 +0900

    view jsp set

--- src/main/java/com/example/sampleproject/HelloController.java
+++ src/main/java/com/example/sampleproject/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.sampleproject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+// @RestController
 public class HelloController {
 
   @RequestMapping("/")
-  public String index() {
-    return "Hallo!";
+  public String index(
+    @RequestParam(name="name",required=false,defaultValue="World")
+    String name, 
+      Model model) {
+    model.addAttribute("name", name);
+    // return "Hallo!";
+    return "index";
   }
   
 }
---
+++ src/main/resources/webapp/WEB-INF/template/index.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Hello, ${name}
+    </title>
+  </head>
+  <body>
+    <div>
+      Hello, ${name}
+    </div>
+  </body>
+</html>
